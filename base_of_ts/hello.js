4a35104c8ac6d211e254f857991984bf174766bb
Teppei0830 <teppei0830.4869@gmail.com>
2023-06-17 18:46:36 +0900

    初めてコミットします。
    typescript と React/Next.js で作る実践 Web アプリケーション開発の本を読みながら
    練習をしています。

---
+++ base_of_ts/hello.js
@@ -0,0 +1,5 @@
+function sayHello(firstname) {
+    console.log("Hello " + firstname);
+}
+var firstname = "Takuya";
+sayHello(firstname);
---
+++ base_of_ts/hello.ts
@@ -0,0 +1,6 @@
+function sayHello(firstname: string) {
+  console.log("Hello " + firstname);
+}
+
+let firstname: string = "Takuya";
+sayHello(firstname);
