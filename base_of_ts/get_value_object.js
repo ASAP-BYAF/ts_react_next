f72150ed4617093762c53898099d1d09ba58bc14
teppei <teppei0830.4869@gmail.com>
2023-06-19 22:49:16 +0900

    base_of_ts に新たにファイルを追加しました。
    これらは、 object の値を取得する練習用のプログラムです。

---
+++ base_of_ts/get_value_object.js
@@ -0,0 +1,6 @@
+var myObject = {
+    name: "teppei",
+    age: 23,
+};
+var name = myObject.name;
+console.log({ name: name });
---
+++ base_of_ts/get_value_object.ts
@@ -0,0 +1,7 @@
+const myObject: { name: string; age: number } = {
+  name: "teppei",
+  age: 23,
+};
+
+const { name } = myObject;
+console.log({ name });
