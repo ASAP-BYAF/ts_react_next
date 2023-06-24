792759de37cf3c205b775fe232cf5b01eaf8fb07
teppei <teppei0830.4869@gmail.com>
2023-06-24 21:19:58 +0900

    StyledButton.stories.tsx を編集しました。
    メタデータオブジェクトに argTypes を追加して、クリック時の動作を追加しました。
    同時に各コンポーネントの表記を関数コンポーネントから StoryObj 型に変更しました。
    関数コンポーネントの表記にしているとメタデータオブジェクトに対する変更がうまく反映されなかったためです。

--- next-sample2/stories/StyledButton.stories.tsx
+++ next-sample2/stories/StyledButton.stories.tsx
@@ -5,20 +5,34 @@ import { StyledButton, StyledButtonProps } from "../components/StyledButton";
 const meta: Meta<typeof StyledButton> = {
   title: "Example/StyledButton",
   component: StyledButton,
+  argTypes: {
+    onClick: {
+      action: "clicked",
+    },
+  },
 };
 
 export default meta;
-// type Story = StoryObj<StyledButtonProps>;
+type Story = StoryObj<typeof StyledButton>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary = () => {
-  return <StyledButton variant="primary">Primary</StyledButton>;
+export const Primary: Story = {
+  args: {
+    variant: "primary",
+    children: "Primary",
+  },
 };
 
-export const Success = () => {
-  return <StyledButton variant="success">Success</StyledButton>;
+export const Success: Story = {
+  args: {
+    variant: "success",
+    children: "success",
+  },
 };
 
-export const Transparent = () => {
-  return <StyledButton variant="transparent">Transparent</StyledButton>;
+export const Transparent: Story = {
+  args: {
+    variant: "transparent",
+    children: "transparent",
+  },
 };
