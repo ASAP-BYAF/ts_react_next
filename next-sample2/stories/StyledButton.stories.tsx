fcadc5c43959f2ae4d94c03c96ce020627d30b51
teppei <teppei0830.4869@gmail.com>
2023-06-24 21:44:30 +0900

    StyledButton.stories.tsx を編集しました。
    コンポーネントの表記を関数形式に直し、 Primary ボタンに
    useState を使った action を設定しました。

--- next-sample2/stories/StyledButton.stories.tsx
+++ next-sample2/stories/StyledButton.stories.tsx
@@ -1,38 +1,38 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { StyledButton, StyledButtonProps } from "../components/StyledButton";
+import { useState } from "react";
+import { action } from "@storybook/addon-actions";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof StyledButton> = {
   title: "Example/StyledButton",
   component: StyledButton,
-  argTypes: {
-    onClick: {
-      action: "clicked",
-    },
-  },
 };
 
 export default meta;
-type Story = StoryObj<typeof StyledButton>;
+// type Story = StoryObj<StyledButtonProps>;
+
+const incrementAction = action("increment");
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
-  args: {
-    variant: "primary",
-    children: "Primary",
-  },
+export const Primary = () => {
+  const [count, setCount] = useState(0);
+  const onClick = (e: React.MouseEvent) => {
+    incrementAction(e, count);
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <StyledButton variant="primary" onClick={onClick}>
+      Count: {count}
+    </StyledButton>
+  );
 };
 
-export const Success: Story = {
-  args: {
-    variant: "success",
-    children: "success",
-  },
+export const Success = () => {
+  return <StyledButton variant="success">Success</StyledButton>;
 };
 
-export const Transparent: Story = {
-  args: {
-    variant: "transparent",
-    children: "transparent",
-  },
+export const Transparent = () => {
+  return <StyledButton variant="transparent">Transparent</StyledButton>;
 };
