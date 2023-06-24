98f601d60450ce8e728d3acfcdda70140563d096
teppei <teppei0830.4869@gmail.com>
2023-06-24 22:08:09 +0900

    stories/StyledButton.stories.tsx を編集しました。
    args を meta データからまとめて与えるようにしました。
    最初の読み込み時は variant が選択されていないのでエラーになりますが、
    ラジオボタンから選択すればうまく表示を切り替えられます。
    今後、  StyledButton コンポーネントの引数に初期値を与えることで
    最初の読み込み時のエラーをなくせると考えています。

--- next-sample2/stories/StyledButton.stories.tsx
+++ next-sample2/stories/StyledButton.stories.tsx
@@ -1,38 +1,32 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { StyledButton, StyledButtonProps } from "../components/StyledButton";
-import { useState } from "react";
-import { action } from "@storybook/addon-actions";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof StyledButton> = {
   title: "Example/StyledButton",
   component: StyledButton,
+  argTypes: {
+    variant: {
+      control: { type: "radio" },
+      options: ["primary", "success", "transparent"],
+    },
+
+    children: {
+      control: { type: "text" },
+    },
+
+    onClick: {
+      action: "clicked",
+    },
+  },
 };
 
 export default meta;
-// type Story = StoryObj<StyledButtonProps>;
-
-const incrementAction = action("increment");
+type Story = StoryObj<typeof StyledButton>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary = () => {
-  const [count, setCount] = useState(0);
-  const onClick = (e: React.MouseEvent) => {
-    incrementAction(e, count);
-    setCount((c) => c + 1);
-  };
+export const Primary: Story = {};
 
-  return (
-    <StyledButton variant="primary" onClick={onClick}>
-      Count: {count}
-    </StyledButton>
-  );
-};
+export const Success: Story = {};
 
-export const Success = () => {
-  return <StyledButton variant="success">Success</StyledButton>;
-};
-
-export const Transparent = () => {
-  return <StyledButton variant="transparent">Transparent</StyledButton>;
-};
+export const Transparent: Story = {};
