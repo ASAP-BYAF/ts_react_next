0abbf90b0be3be1731619ae5857353bcb82fbd68
teppei <teppei0830.4869@gmail.com>
2023-06-25 12:18:15 +0900

    新たに base_of_ts 下に interface.ts を作成しました。
    これは、 interface の使用を確認するための練習用のコードです。

---
+++ base_of_ts/interface.js
@@ -0,0 +1,5 @@
+var point = {
+    x: 1,
+    y: 2,
+};
+console.log("x = ".concat(point.x, ", y = ").concat(point.y));
---
+++ base_of_ts/interface.ts
@@ -0,0 +1,11 @@
+interface Point {
+  x: number;
+  y: number;
+}
+
+const point2: Point = {
+  x: 1,
+  y: 2,
+};
+
+console.log(`x = ${point2.x}, y = ${point2.y}`);
